7ccc70f74d52cb3d0589f047a32c98edded31ed5
Orso Forghieri <59018744+OrsoF@users.noreply.github.com>
2022-04-07 17:21:33 +0200

    Final

--- kernels.py
+++ kernels.py
@@ -43,3 +43,141 @@ class chi2:
                 out[i, j] = -p
         tmp = self.gamma * out
         return  np.exp(tmp, tmp)
+        
+# Kernel SVC classifier for binary classification as in the correction of hwk 3 :
+
+class KernelSVC_Binary:
+    
+    def __init__(self, C, kernel, epsilon = 1e-3):
+        self.type = 'non-linear'
+        self.C = C                               
+        self.kernel = kernel        
+        self.alpha = None
+        self.support = None
+        self.epsilon = epsilon
+        self.norm_f = None
+       
+    
+    def fit(self, X, y):
+       #### You might define here any variable needed for the rest of the code
+        N = len(y)
+        hXX = self.kernel(X, X)
+        G = np.einsum('ij,i,j->ij',hXX,y,y)
+        A = np.vstack((-np.eye(N), np.eye(N)))             
+        b = np.hstack((np.zeros(N), self.C * np.ones(N)))  
+
+        # Lagrange dual problem
+        def loss(alpha):
+            return -alpha.sum() + 0.5 * alpha.dot(alpha.dot(G))  #'''--------------dual loss ------------------ '''
+
+        # Partial derivate of Ld on alpha
+        def grad_loss(alpha):
+            return -np.ones_like(alpha) + alpha.dot(G) # '''----------------partial derivative of the dual loss wrt alpha-----------------'''
+
+
+        # Constraints on alpha of the shape :
+        # -  d - C*alpha  = 0
+        # -  b - A*alpha >= 0
+
+        fun_eq = lambda alpha:  np.dot(alpha, y) # '''----------------function defining the equality constraint------------------'''        
+        jac_eq = lambda alpha:   y  #'''----------------jacobian wrt alpha of the  equality constraint------------------'''
+        fun_ineq = lambda alpha:  b - np.dot(A, alpha) # '''---------------function defining the ineequality constraint-------------------'''     
+        jac_ineq = lambda alpha:  -A # '''---------------jacobian wrt alpha of the  inequality constraint-------------------'''
+        
+        constraints = ({'type': 'eq',  'fun': fun_eq, 'jac': jac_eq},
+                       {'type': 'ineq', 
+                        'fun': fun_ineq , 
+                        'jac': jac_ineq})
+
+        optRes = optimize.minimize(fun=lambda alpha: loss(alpha),
+                                   x0=np.ones(N), 
+                                   method='SLSQP', 
+                                   jac=lambda alpha: grad_loss(alpha), 
+                                   constraints=constraints)
+        self.alpha = optRes.x
+
+        ## Assign the required attributes
+        
+        margin_pointsIndices = (self.alpha > self.epsilon)
+        boundaryIndices = (self.alpha > self.epsilon) * (self.C- self.alpha > self.epsilon )
+        
+        self.support = X[boundaryIndices] #'''------------------- A matrix with each row corresponding to a support vector ------------------'''
+        
+        self.margin_points = X[margin_pointsIndices]
+        self.margin_points_AlphaY = y[margin_pointsIndices] * self.alpha[margin_pointsIndices]
+        
+        self.b = y[boundaryIndices][0] - self.separating_function(np.expand_dims(X[boundaryIndices][0],axis=0)) #''' -----------------offset of the linear classifier------------------ '''
+        K_margin_points = self.kernel(self.margin_points, self.margin_points)
+        self.norm_f = np.einsum('i,ij,j->', self.margin_points_AlphaY , K_margin_points, self.margin_points_AlphaY)
+
+
+    ### Implementation of the separting function $f$ 
+    def separating_function(self,x):
+        # Input : matrix x of shape N data points times d dimension
+        # Output: vector of size N
+        x1 = self.kernel(self.margin_points, x)
+        return np.einsum('ij,i->j',x1,self.margin_points_AlphaY)
+    
+    
+    def predict(self, X):
+        """ Predict y values in {-1, 1} """
+        d = self.separating_function(X)
+        return 2 * (d+self.b> 0) - 1
+        
+class KernelSVC_OneVsOne :
+    
+    def __init__(self, num_classes, C, kernel, epsilon = 1e-3):
+        self.type = 'non-linear'
+        self.num_classes = num_classes
+        self.pool = {} # contain the classifiers for all classes
+        for i in range(self.num_classes) : 
+            for j in range(i+1, self.num_classes) : 
+                self.pool[(i,j)] = KernelSVC_Binary(C=C, kernel=kernel, epsilon=epsilon)
+       
+    def fit(self, X, y):
+        m = self.num_classes
+        indices_c = {}
+        for i in range(m) : 
+            indices_c[i] = np.argwhere(y==i).reshape(-1)
+
+        progress_bar = tqdm_auto(range(m * (m-1) // 2))
+        for i in range(m) : 
+            for j in range(i+1, m) : 
+                indices_ij = np.concatenate((indices_c[i],indices_c[j]))
+                self.pool[(i,j)].fit(X[indices_ij], 2*(y[indices_ij]==i)-1)   
+                progress_bar.update(1)   
+    
+    def predict(self, X):
+        """ Predictions consist in choosing the category that has the higher number of votes """
+        m = self.num_classes
+        y = np.zeros((len(X), m))
+
+        for i in range(m) : 
+            for j in range(i+1, m) :
+                classif_ij = self.pool[(i,j)].separating_function(X) + self.pool[(i,j)].b 
+                classif_ij = 1*(classif_ij > 0) # 0 if class j and 1 if class i
+                y[:,i] += classif_ij
+                y[:,j] += 1 - classif_ij
+
+        return y.argmax(axis=-1)
+
+class KernelSVC_OneVsRest :
+    
+    def __init__(self, num_classes, C, kernel, epsilon = 1e-3):
+        self.type = 'non-linear'
+        self.num_classes = num_classes
+        self.pool = {} # contain the classifiers for all classes
+        for i in range(self.num_classes) :  
+            self.pool[i] = KernelSVC_Binary(C=C, kernel=kernel, epsilon=epsilon)
+       
+    def fit(self, X, y):
+        for i in tqdm(range(self.num_classes)) : 
+            self.pool[i].fit(X, 2*(y==i)-1)
+    
+    def predict(self, X):
+        """ Predictions consist in choosing the category for which the classifier gives the highest value of the decision function"""
+        y = np.zeros((len(X), self.num_classes))
+        for i in range(self.num_classes) :
+            y[:,i] = self.pool[i].separating_function(X) + self.pool[i].b  # value of the decision function
+  
+        return y.argmax(axis=-1)
--- start.py
+++ start.py
@@ -1,10 +1,29 @@
-!pip install kaggle
-!kaggle competitions download -c mva-mash-kernel-methods-2021-2022
-
 import pandas as pd
 from sift import SIFT
+from kernels import KernelSVC_OneVsRest, chi2
+from os.path import exists
+import zipfile
+import numpy as np
 
-Xtr = np.array(pd.read_csv('mva-mash-kernel-methods-2021-2022/Xtr.csv',header=None,sep=',',usecols=range(3072)))
-Xte = np.array(pd.read_csv('mva-mash-kernel-methods-2021-2022/Xte.csv',header=None,sep=',',usecols=range(3072)))
-Ytr = np.array(pd.read_csv('mva-mash-kernel-methods-2021-2022/Ytr.csv',sep=',',usecols=[1])).squeeze()
+if not exists('mva-mash-kernel-methods-2021-2022'):
+    with zipfile.ZipFile('mva-mash-kernel-methods-2021-2022.zip', 'r') as zip_ref:
+        zip_ref.extractall('mva-mash-kernel-methods-2021-2022')
 
+Xtr = np.array(pd.read_csv('mva-mash-kernel-methods-2021-2022\\mva-mash-kernel-methods-2021-2022\\Xtr.csv',header=None,sep=',',usecols=range(3072)))[:3]
+Xte = np.array(pd.read_csv('mva-mash-kernel-methods-2021-2022\\mva-mash-kernel-methods-2021-2022\\Xte.csv',header=None,sep=',',usecols=range(3072)))[:3]
+Ytr = np.array(pd.read_csv('mva-mash-kernel-methods-2021-2022\\mva-mash-kernel-methods-2021-2022\\Ytr.csv',sep=',',usecols=[1])).squeeze()[:3]
+
+
+sift = SIFT()
+X_train_sift, y_train_sift = sift.get_features_from_data(Xtr.reshape((Xtr.shape[0], 32, 32, 3))), Ytr
+X_test_sift = sift.get_features_from_data(Xte.reshape((Xte.shape[0], 32, 32, 3)))
+
+n_classes = len(set(Ytr))
+
+clf = KernelSVC_OneVsRest(num_classes = n_classes, C = 0.5, kernel = chi2(gamma = 1.).kernel)
+clf.fit(X_train_sift, y_train_sift)
+
+Yte = {'Prediction' : clf.predict(X_test_sift)}
+dataframe = pd.DataFrame(Yte)
+dataframe.index += 1
+dataframe.to_csv('Yte.csv',index_label='Id')
